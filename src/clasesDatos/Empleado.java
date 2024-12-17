fe44e9027317a362ea1c55eda17138e129153c10
Nahia Beraza <nahia.beraza06@somo.eus>
2024-12-17 11:43:23 +0100

    Añadidos CONSTRUSCTORES.

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -8,4 +8,60 @@ public class Empleado {
 	private int edad;
 	private boolean clubSocial; //indica si el empleado está en el club social de la empresa
 	private float sueldo;
+	
+	///////////////////////////CONSTRUCTORES////////////////////////////
+	// CONSTRUCTOR COMPLETO
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellidos
+	 * @param categoria
+	 * @param edad
+	 * @param clubSocial indica si el empleado está en el club social de la empresa
+	 * @param sueldo
+	 */
+	public Empleado(String dni, String nombre, String apellidos, String categoria, int edad, boolean clubSocial,
+			float sueldo) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.categoria = categoria;
+		this.edad = edad;
+		this.clubSocial = clubSocial;
+		this.sueldo = sueldo;
+	}
+	
+	//CONSTRUCTOR VACÍO
+	public Empleado() {
+		super();
+		this.dni = "";
+		this.nombre = "............";
+		this.apellidos = ".............";
+		this.categoria = "Base";
+		this.edad = 0;
+		this.clubSocial = false;
+		this.sueldo = (float) 0.0; 
+	}
+
+	//CONSTRUCTOR PARCIAL
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellidos
+	 */
+	public Empleado(String dni, String nombre, String apellidos) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.categoria = "Base";
+		this.edad = 0;
+		this.clubSocial = false;
+		this.sueldo = (float) 0.0; 
+	}
+	
+	
 }
+
+
